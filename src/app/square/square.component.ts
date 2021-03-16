7341d4be6c82c9c742b77c5460ed33a49632fa5b
Alena Gritsiuk <alena22292@mail.ru>
2021-03-16 12:43:59 +0100

    Create a page with a timer

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
+  num:number = Math.floor(Math.random() * 10);
+  date = new Date();
 
+  // Constructor mostly using for server
+  constructor() {
+    setInterval(() => this.date = new Date(), 100);
+  }
 }
